540ed63dab52082d72e4212210e07f45b19dca52
Abrar2003 <spartanabrar@gmail.com>
2023-04-25 10:19:19 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,18 +8,19 @@ const companyRouter = require("./features/company/company.router");
 const taskRouter = require("./features/tasks/task.router");
 
 const cors = require("cors");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
 let PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 // app.use(function (req, res, next) {
 //   //Enabling CORS
 //   res.header("Access-Control-Allow-Origin", "*");
